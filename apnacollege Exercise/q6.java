fd75578115f73c2ba5ce602e3341463e9d943e10
Arafat Sany <human.sany7@gmail.com>
2025-02-02 01:50:06 +0600

    6th

---
+++ apnacollege Exercise/q6.java
@@ -0,0 +1,12 @@
+
+public class q6 {
+
+    public static void main(String[] args) {
+        int i = 5;
+
+        do {
+            System.out.println("Infinity");
+
+        } while (i > 0);
+    }
+}
