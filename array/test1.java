018fd0d7754071acdb6a2b2fbe58f17d91b12fc8
Arafat Sany <human.sany7@gmail.com>
2025-02-04 01:26:02 +0600

    array1

---
+++ array/test1.java
@@ -0,0 +1,14 @@
+
+public class test1 {
+
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+
+        marks[0] = 97;
+        marks[1] = 96;
+        marks[2] = 43;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
