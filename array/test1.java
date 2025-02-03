fb45fa28e09cd00a91a7296e7544a50cf7eb42d0
Arafat Sany <human.sany7@gmail.com>
2025-02-04 01:53:01 +0600

    array2

--- array/test1.java
+++ array/test1.java
@@ -1,14 +1,31 @@
 
+import java.util.Scanner;
+
 public class test1 {
 
     public static void main(String[] args) {
-        int[] marks = new int[3];
 
-        marks[0] = 97;
-        marks[1] = 96;
-        marks[2] = 43;
-        for (int i = 0; i < 3; i++) {
-            System.out.println(marks[i]);
+        // int[] marks = new int[3];
+        // marks[0] = 97;
+        // marks[1] = 96;
+        // marks[2] = 43;
+        // int[] marks = {97, 95, 92};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size");
+        int size = sc.nextInt();
+        int[] marks = new int[size];
+
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < size; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            if (marks[i] == x) {
+                System.out.println("X found at index : " + i);
+            }
         }
     }
 }
